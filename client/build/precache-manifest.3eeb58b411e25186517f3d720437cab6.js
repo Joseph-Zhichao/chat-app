756d6a52fc6400e853347fa1a0cde6d8ff686efb
Joseph ZHU <josephzhu@Josephs-MacBook-Pro.local>
2020-10-19 17:15:23 -0700

    first commit

---
+++ client/build/precache-manifest.3eeb58b411e25186517f3d720437cab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc689a835b6d541a8edc6de332ba3270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0606988abb42aeaec278",
+    "url": "/static/css/main.f9a6f996.chunk.css"
+  },
+  {
+    "revision": "7f5129ddcbee662f15bd",
+    "url": "/static/js/2.f024598a.chunk.js"
+  },
+  {
+    "revision": "c69fcf8670b313a5daf5b087e062c182",
+    "url": "/static/js/2.f024598a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0606988abb42aeaec278",
+    "url": "/static/js/main.68773a8e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
